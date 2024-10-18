5ff0b6c35fd330858fc5bb2ef056e50620f092db
andrew-surrena <andrewsurrena@gmail.com>
2024-10-17 20:45:07 -0700

    all roads lead to production

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,31 +16,32 @@ const startApolloServer = async () => {
   await server.start();
   await db();
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+  const app = express();
+  const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-app.use('/graphql', expressMiddleware(server as any,
-  {
-    context: authenticateToken as any
+  app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }
+  ));
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(process.cwd(), '../client/dist')));
   }
-));
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(process.cwd(), '../client/dist')));
-
   app.get('*', (_req, res) => {
-    res.sendFile(path.join(process.cwd(),'../client/dist/index.html'));
+    res.sendFile(path.join(process.cwd(), '../client/dist/index.html'));
   });
-}
 
 
-  app.listen(PORT, () => {console.log(`API server running on port ${PORT}!`);
-  console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-});
+
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
 };
 
 startApolloServer();
