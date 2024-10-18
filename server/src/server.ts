1dee68e42bbe157d5e8846f44785a366b9fd8949
andrew-surrena <andrewsurrena@gmail.com>
2024-10-17 20:33:40 -0700

    updated connection path

--- server/src/server.ts
+++ server/src/server.ts
@@ -30,10 +30,10 @@ app.use('/graphql', expressMiddleware(server as any,
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(process.cwd(), '../client/dist')));
 
   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile(path.join(process.cwd(),'../client/dist/index.html'));
   });
 }
 
